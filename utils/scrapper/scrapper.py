fa97be30277338d63e7dafffd6234e9f2e525074
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-05-11 22:38:59 +0500

    Completed scrapper script

--- utils/scrapper/scrapper.py
+++ utils/scrapper/scrapper.py
@@ -2,6 +2,7 @@ import sys
 import os
 import requests
 import hashlib
+import json
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -9,31 +10,45 @@ from bs4 import BeautifulSoup
 
 
 URL = "https://www.cameo.com/"
+NEWLINE = "\n"
 SHA256SUMS = "SHA256SUMS"
+MAX_TOKEN_LINES = 91
+TOKEN_ENCODING = "utf-8"
 BACKUP = "backup.html"
+SOUP_PARSER = "lxml"
+STAR_CARD_TAG = "div"
+STAR_CARD_SELECTOR = "data-testid"
+STAR_CARD_VALUE = "undefined-StarCard"
+PROFILE_IMAGE_TAG = "img"
 PROFILE_IMAGE_CLASS = "css-9pa8cd"
+TALENT_NAME_TAG = "div"
+TALENT_NAME_SELECTOR = "data-testid"
+TALENT_NAME_VALUE = "talent-name"
+LIMIT = 30
+IMAGE_HEIGHT = 320
+JSON_IMAGE_ATTR = "profile_image"
+JSON_NAME_ATTR = "talent_name"
+JSON_PROFESSION_ATTR = "talent_profession"
+DUMP_FILE = "../../src/assets/data/talent.json"
 
 
 def get_dynamic_page_source(hash):
-    print("Adding \"--headless\", \"--disable-extensions\", \"--disable-gpu\" arguments to chrome options")
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-gpu")
 
-    print("Starting chrome browser in headless state")
+    # FIXME: Issue #1234: Adding --headless, --disable-extensions, and --disable-gpu options caused selenium to not fetch complete page source
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-extensions")
+    # chrome_options.add_argument("--disable-gpu")
+
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(URL)
     page_source = driver.page_source
-    print("Shutting down chrome and webdriver")
     driver.quit()
 
-    print("Writing fetched page source to BACKUP file")
     with open(BACKUP, "w") as file:
         file.write(page_source)
         file.close()
 
-    print("Writing current hash to SHA256SUMS file")
     with open(SHA256SUMS, "w") as file:
         file.write(hash)
         file.close()
@@ -42,7 +57,6 @@ def get_dynamic_page_source(hash):
 
 
 def get_backup_page_source():
-    print("Reading page source from BACKUP file")
     with open(BACKUP, "r") as file:
         page_source = file.read()
         file.close()
@@ -50,9 +64,11 @@ def get_backup_page_source():
         return page_source
 
 
-def get_page_source():
-    print("Creating token and hash from requests GET method")
-    token = "\n".join(requests.get(URL).text.split("\n")[:91]).encode("utf-8")
+def get_page_source(only_backup=False):
+    if only_backup:
+        return get_backup_page_source()
+
+    token = NEWLINE.join(requests.get(URL).text.split(NEWLINE)[:MAX_TOKEN_LINES]).encode(TOKEN_ENCODING)
     hash = hashlib.sha256(token).hexdigest()
 
     request_selenium = False
@@ -68,34 +84,64 @@ def get_page_source():
         with open(SHA256SUMS, "r") as file:
             sha256sums = file.read()
 
-            print(f"SHA256SUMS: {sha256sums}")
-            print(f"HASH: {hash}")
-
             if sha256sums != hash:
                 request_selenium = True
-                print("SHA256SUMS exists but does not matches current hash")
-            else:
-                print("SHA256SUMS exists and matches the current hash")
 
             file.close()
     else:
-        print("SHA256SUMS does not exist")
         request_selenium = True
 
     if not os.path.isfile(BACKUP):
-        print("BACKUP does not exist")
         request_selenium = True
 
-    print("Requesting dynamic page source from selenium") if request_selenium else print("Opening backup page source")
-    page_source = get_dynamic_page_source(hash) if request_selenium else get_backup_page_source()
+    return get_dynamic_page_source(hash) if request_selenium else get_backup_page_source()
 
-    return page_source
+
+def is_star_card(tag):
+    return tag.name == STAR_CARD_TAG and tag.has_attr(STAR_CARD_SELECTOR) and tag.get(STAR_CARD_SELECTOR) == STAR_CARD_VALUE and tag.find(PROFILE_IMAGE_TAG, class_=PROFILE_IMAGE_CLASS) is not None
+
+
+def get_talent_list(page_source):
+    soup = BeautifulSoup(page_source, SOUP_PARSER)
+    star_cards = soup.find_all(is_star_card, limit=LIMIT)
+
+    talent_list = []
+
+    for star_card in star_cards:
+        profile_image_element = star_card.find(PROFILE_IMAGE_TAG, class_=PROFILE_IMAGE_CLASS)
+        profile_image = profile_image_element.get("src")
+        query_start = profile_image.rfind("?")
+        height_start = profile_image.find("=", query_start)
+        height_end = profile_image.find("&", query_start)
+        height = str(IMAGE_HEIGHT)
+        profile_image = profile_image[:height_start + 1] + height + profile_image[height_end:]
+
+        talent_name_element = star_card.find(TALENT_NAME_TAG, attrs={TALENT_NAME_SELECTOR: TALENT_NAME_VALUE})
+        talent_name = talent_name_element.string.strip()
+
+        talent_profession_element = talent_name_element.next_sibling
+        talent_profession = talent_profession_element.string.strip()
+
+        talent = {}
+        talent[JSON_IMAGE_ATTR] = profile_image
+        talent[JSON_NAME_ATTR] = talent_name
+        talent[JSON_PROFESSION_ATTR] = talent_profession
+        talent_list.append(talent)
+
+    return talent_list
+
+
+def dump_json(talent_list):
+    with open(DUMP_FILE, "w") as file:
+        json.dump(talent_list, file, indent=2)
 
 
 def main():
-    print("Getting page source from suitable mechanism")
-    page_source = get_page_source()
-    soup = BeautifulSoup(page_source, "lxml")
+    # FIXME: Temporarily disabled fetching from selenium because of issue: 1234
+
+    page_source = get_page_source(only_backup=True)
+    talent_list = get_talent_list(page_source)
+    dump_json(talent_list)
 
     return 0
 
