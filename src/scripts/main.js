6bb67eaa0077dcb2cf3832d44a1d2672145145ae
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-05-12 09:07:29 +0500

    Completed filter functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,11 @@ async function populateSlides() {
   // Test if browser supports HTML5 template element by checking for the presence of template element's content attribute
 
   if (!("content" in document.createElement("template"))) {
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent = "Your browser does not support HTML5 template elements!";
+    document.querySelector(".root").removeChild(document.querySelector(".gallery"));
+    document.querySelector(".root").appendChild(errorMessage);
+
     return;
   }
 
@@ -18,10 +23,49 @@ async function populateSlides() {
     slideClone.querySelector(".slide--picture").src = talent["profile_image"];
     slideClone.querySelector(".slide--name").textContent = talent["talent_name"];
     slideClone.querySelector(".slide--description").textContent = talent["talent_profession"];
+    initializeToolbar(slideClone);
     gallery.appendChild(slideClone);
   }
 
   initializeCarousel();
+}
+
+function initializeToolbar(slide) {
+  const tools = slide.querySelectorAll(".slide--toolbar button");
+  const scale = slide.querySelector(".slide--scale");
+
+  scale.addEventListener("input", () => {
+    const activeTool = slide.querySelector(".active");
+    const activeClass = activeTool.classList[0];
+    const slidePicture = slide.querySelector(".slide--picture");
+    const filters = { blur: "blur", brightness: "brightness", contrast: "contrast", grayscale: "grayscale", huerotate: "hue-rotate" };
+    const units = { blur: "px", brightness: "%", contrast: "%", grayscale: "%", huerotate: "deg" };
+    slidePicture.style.filter = `${filters[activeClass]}(${scale.value}${units[activeClass]})`;
+  });
+
+  for (const tool of tools) {
+    tool.addEventListener("click", (e) => {
+      for (const anyTool of tools) {
+        anyTool.classList.remove("active");
+      }
+
+      const currentTool = e.currentTarget;
+      currentTool.classList.add("active");
+
+      const maxValues = { blur: 20, brightness: 200, contrast: 200, grayscale: 100, huerotate: 360 };
+      const stepValues = { blur: 2, brightness: 20, contrast: 20, grayscale: 10, huerotate: 36 };
+      const defaultValues = { blur: 0, brightness: 100, contrast: 100, grayscale: 0, huerotate: 0 };
+      const toolClass = currentTool.classList[0];
+
+      scale.setAttribute("max", maxValues[toolClass]);
+      scale.setAttribute("step", stepValues[toolClass]);
+      scale.value = defaultValues[toolClass];
+
+      e.stopPropagation();
+    });
+  }
+
+  tools[0].classList.add("active");
 }
 
 function activateCarousel(index) {
@@ -32,13 +76,7 @@ function activateCarousel(index) {
     return;
   }
 
-  if (slide.classList.contains("rqueue")) {
-    slide.classList = "slide active";
-  }
-
-  if (slide.classList.contains("lqueue")) {
-    slide.classList = "slide active";
-  }
+  slide.classList = "slide active";
 
   if (slides[index - 2] != undefined) {
     slides[index - 2].classList = "slide lhidden";
@@ -56,6 +94,7 @@ function activateCarousel(index) {
 
 function initializeCarousel() {
   const slides = document.querySelectorAll(".slide");
+
   for (let index = 0; index < slides.length; index++) {
     const slide = slides[index];
     slide.classList.add("rhidden");
@@ -63,6 +102,7 @@ function initializeCarousel() {
       activateCarousel(index);
     });
   }
+
   slides[0].classList = "slide active";
   slides[1].classList = "slide rqueue";
 }
